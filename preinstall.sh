642cf91a73ce6d3b7d054d75ed3d384c2afcd9f1
Abdullah Sahin <abdullah@sahin.uk>
2019-06-04 21:32:06 +0200

    preinstall ubuntu 18.04

---
+++ preinstall.sh
@@ -0,0 +1,19 @@
+sudo apt-get update
+sudo apt-get upgrade
+
+sudo apt-get install libprotobuf-dev libleveldb-dev libsnappy-dev libopencv-dev libhdf5-serial-dev protobuf-compiler
+sudo apt-get install --no-install-recommends libboost-all-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libopenblas-dev
+sudo apt-get install libgflags-dev libgoogle-glog-dev liblmdb-dev
+
+
+
+echo "\n# caffe-posenet preinstall.sh" >> ~/.bashrc
+echo "export CAFFE_ROOT=$(pwd)" >> ~/.bashrc
+echo "export LD_LIBRARY_PATH=\$LD_LIBRARY_PATH:/usr/local/cuda/lib64" >> ~/.bashrc
+echo "export PYTHONPATH=\$CAFFE_ROOT/python:\$PYTHONPATH" >> ~/.bashrc
+echo "# end caffe-posenet preinstall.sh\n" >> ~/.bashrc
+
+source ~/.bashrc
+
